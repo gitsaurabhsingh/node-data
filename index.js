244e1fcd08aaf76742866997df6491a3e12b06b4
gitsaurabhsingh <saurabhsurvanshi2002@gmail.com>
2024-08-16 03:59:51 -0700

    hello

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const dataItem1 = fs.readFileSync(`${__dirname}/Data/header.json`, "utf-8");
 const dataApi1 = JSON.parse(dataItem1);
 const dataItem2 = fs.readFileSync(`${__dirname}/Data/banner.json`, "utf-8");
 const dataApi2 = JSON.parse(dataItem2);
+const dataItem3 = fs.readFileSync(`${__dirname}/Data/futured.json`, "utf-8");
+const dataApi3 = JSON.parse(dataItem3);
+const dataItem4 = fs.readFileSync(`${__dirname}/Data/Footer.json`, "utf-8");
+const dataApi4 = JSON.parse(dataItem4);
 
 app.get("/", (req, res) => {
   res.send(dataApi);
@@ -24,6 +28,12 @@ app.get("/header", (req, res) => {
 });
 app.get("/banner", (req, res) => {
   res.send(dataApi2);
+});
+app.get("/futured", (req, res) => {
+  res.send(dataApi3);
+});
+app.get("/footer", (req, res) => {
+  res.send(dataApi4);
 });
 app.listen(port, () => {
   console.log(`localhost//:${port}`);
